6a336d17954dffa01fa4529d16ec9e305c2bc829
rgallay <robin.gallay@students.hevs.ch>
2021-11-27 09:00:55 +0100

    filterOK

---
+++ ImageEdgeDetectionUnitTest/FilterTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using ImageEdgeDetection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageEdgeDetectionUnitTest
+{
+    [TestClass]
+    public class FilterTest
+    {
+        [TestMethod]
+        public void TestBlackAndWhiteFilter()
+        {
+            //base
+            Bitmap basePicture = new Bitmap(@"..\\..\\Images\\mario-base.png");
+            string resultImageAfterFilter_REF;
+            string resultSavedBitmap_REF;
+
+            //action
+            Bitmap resultImageAfterFilter = ImageFilters.BlackWhite(basePicture);
+            Bitmap resultSavedBitmap = new Bitmap(@"..\\..\\Images\\mario-blackwhite.png");
+
+            //Assert
+            if (resultImageAfterFilter.Width == resultSavedBitmap.Width && resultImageAfterFilter.Height == resultSavedBitmap.Height)
+            {
+                for (int i = 0; i < resultImageAfterFilter.Width; i++)
+                {
+                    for (int j = 0; j < resultImageAfterFilter.Height; j++)
+                    {
+                        resultImageAfterFilter_REF = resultImageAfterFilter.GetPixel(i, j).ToString();
+                        resultSavedBitmap_REF = resultSavedBitmap.GetPixel(i, j).ToString();
+
+                        Assert.AreEqual(resultImageAfterFilter_REF, resultSavedBitmap_REF);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestRainbowFilter()
+        {
+            //base
+            Bitmap basePicture = new Bitmap(@"..\\..\\Images\\mario-base.png");
+            string resultImageAfterFilter_REF;
+            string resultSavedBitmap_REF;
+
+            //action
+            Bitmap resultImageAfterFilter = ImageFilters.RainbowFilter(basePicture);
+            Bitmap resultSavedBitmap = new Bitmap(@"..\\..\\Images\\mario-rainbow.png");
+
+            //Assert
+            if (resultImageAfterFilter.Width == resultSavedBitmap.Width && resultImageAfterFilter.Height == resultSavedBitmap.Height)
+            {
+                for (int i = 0; i < resultImageAfterFilter.Width; i++)
+                {
+                    for (int j = 0; j < resultImageAfterFilter.Height; j++)
+                    {
+                        resultImageAfterFilter_REF = resultImageAfterFilter.GetPixel(i, j).ToString();
+                        resultSavedBitmap_REF = resultSavedBitmap.GetPixel(i, j).ToString();
+
+                        Assert.AreEqual(resultImageAfterFilter_REF, resultSavedBitmap_REF);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ImageEdgeDetectionUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageEdgeDetectionUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageEdgeDetectionUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("643c3014-b7e3-4ea9-9b77-0044b0dfe285")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
